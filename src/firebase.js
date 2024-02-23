83e91bafe6420d7de8cb88a983eb125f16b5ffbf
Prshnt Bhushal <prshnt.bhushal@gmail.com>
2024-02-23 18:05:20 +0545

    removed env file and updated firebase.js file

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
-  apiKey: 'AIzaSyC4Q_oGd-Gp60q3uV1PejUOzyq3F6WNyd8',
-  authDomain: 'bookexchage-d8853.firebaseapp.com',
-  projectId: 'bookexchage-d8853',
-  storageBucket: 'bookexchage-d8853.appspot.com',
-  messagingSenderId: '119191781686',
-  appId: '1:119191781686:web:79b64977cfc4870ef27397',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
